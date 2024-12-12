8fc603833d1c1a2013eb2444670b2899bf55ef97
EnriqueRTG <enriqueramontg@gmail.com>
2024-12-12 17:42:25 -0300

    Inicializacion del proyecto en Codeigniter 4. Maquetacion del Dashboard utilizando Bootstrap 5

---
+++ app/Views/layouts/footer.php
@@ -0,0 +1,22 @@
+<footer class="py-4 bg-light mt-auto">
+    <div class="container-fluid px-4">
+        <div class="d-flex align-items-center justify-content-between small">
+            <div class="text-muted">Copyright &copy; <a href="https://github.com/EnriqueRTG" style="text-decoration: none; font-weight: bold;" target="_blank">EnriqueRTG</a> <?php echo date('Y')?>  </div>
+            <div>
+                <a href="https://www.facebook.com" target="_blank"><i class="fa-brands fa-facebook"></i></a>
+                &middot;
+                <a href="https://www.instagram.com/" target="_blank"><i class="fa-brands fa-instagram"></i></a>
+            </div>
+        </div>
+    </div>
+</footer>
+</div>
+</div>
+<script src="assets/js/jquery-3.7.1.min.js"></script>
+<script src="assets/js/bootstrap.bundle.min.js"></script>
+<script src="assets/js/scripts.js"></script>
+<script src="assets/js/datatables-simple-demo.js"></script>
+<script src="assets/js/simple-datatables.min.js"></script>
+</body>
+
+</html>
